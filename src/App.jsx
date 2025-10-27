45df2ad8230892b550ee2c3bac660a7269d2327e
Dhwani Desai <dhwanid11@gmail.com>
2025-10-27 23:48:09 +0000

    fetched movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import "./App.css";
 import ChooseMovie from "./ChooseMovie";
 import Homepage from "./Homepage";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import data from "./movies.json";
 
 function App() {
     const [preferences, setPreferences] = useState(null);
+    const [movies, setMovies] = useState();
+
+    useEffect(() => {
+        if (preferences) {
+            setMovies(data);
+        }
+    }, [preferences]);
 
     const resetPreferences = () => {
         setPreferences(null);
+        setMovies(null);
     };
 
     return (
         <>
-            {!preferences ? (
-                <Homepage setPreferences={setPreferences} />
-            ) : (
-                <ChooseMovie prefs={preferences} handleBackClick={resetPreferences} />
+            {!preferences && <Homepage setPreferences={setPreferences} />}
+            {movies && (
+                <ChooseMovie
+                    movies={movies}
+                    handleBackClick={resetPreferences}
+                />
             )}
         </>
     );
--- src/ChooseMovie.jsx
+++ src/ChooseMovie.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import data from "./movies.json";
 
-export default function ChooseMovie({ prefs, handleBackClick }) {
+export default function ChooseMovie({ movies, handleBackClick }) {
     return (
         <>
             <div>
                 <button onClick={handleBackClick}>Back</button>
-                {prefs.fromYear}
-                {prefs.toYear}
-                {prefs.items}
+                {movies?.map((movie) => (<p key={movie.movieName}>
+                    {movie.movieName}
+                </p>))}
                 <button>Shuffle</button>
             </div>
         </>

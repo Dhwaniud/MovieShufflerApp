f71aa907a603df687f872ae8da0203e30659c741
Dhwani Desai <dhwanid11@gmail.com>
2025-10-28 00:07:17 +0000

    random movie generator

--- src/ChooseMovie.jsx
+++ src/ChooseMovie.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import data from "./movies.json";
 
 export default function ChooseMovie({ movies, handleBackClick }) {
+    const [randomMovie, setRandomMovie] = useState(movies[0]);
+
+    const handleShuffle = useCallback(() => {
+        const random = Math.floor(Math.random() * movies.length);
+        setRandomMovie(movies[random]);
+    }, [movies]);
+
     return (
         <>
             <div>
                 <button onClick={handleBackClick}>Back</button>
-                {movies?.map((movie) => (<p key={movie.movieName}>
-                    {movie.movieName}
-                </p>))}
-                <button>Shuffle</button>
+                <p>{randomMovie?.movieName}</p>
+                <button onClick={handleShuffle}>Shuffle</button>
             </div>
         </>
     );
